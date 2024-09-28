47beb5197a34ef601313ae966c32f5d9956d5eaf
Guðmundur Garðar Árnason <gardar39@gmail.com>
2024-09-28 14:29:11 +0000

    additional js examples

--- Lesson 5/js-beginner/script.js
+++ Lesson 5/js-beginner/script.js
@@ -1,4 +1,5 @@
 // Part 1: Variables & Math Operations
+
 let num1 = 10; // Change this value
 let num2 = 5;  // Change this value
 
@@ -10,6 +11,7 @@ console.log("Division: " + (num1 / num2));
 console.log("Remainder: " + (num1 % num2));
 
 // Part 2: Conditionals
+
 if (num1 > num2) {
     console.log(num1 + " is greater than " + num2);
 } else if (num1 < num2) {
@@ -19,6 +21,7 @@ if (num1 > num2) {
 }
 
 // Part 3: Loops
+
 for (let i = 1; i <= 10; i++) {
     if (i % 2 === 0) {
         console.log(i + " is even");
@@ -28,6 +31,7 @@ for (let i = 1; i <= 10; i++) {
 }
 
 // Part 4: Functions
+
 function multiplyNumbers(a, b) {
     return a * b;
 }
@@ -35,8 +39,8 @@ function multiplyNumbers(a, b) {
 let result = multiplyNumbers(num1, num2);
 console.log("The product of " + num1 + " and " + num2 + " is: " + result);
 
-
 // Bonus: User Input
+
 let userNum1 = parseInt(prompt("Enter the first number:"));
 let userNum2 = parseInt(prompt("Enter the second number:"));
 
