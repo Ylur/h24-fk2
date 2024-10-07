0ac2e6a053ff1424d55842de425690cf296f233b
Ingi Haralds <ingiharaldss@gmail.com>
2024-10-07 18:37:18 +0000

    photos í public

--- hungry-llama/src/components/Hero.js
+++ hungry-llama/src/components/Hero.js
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <div
       className="bg-cover bg-center h-96 flex items-center justify-center text-white"
-      style={{ backgroundImage: 'url("/path-to-hero-image.jpg")' }}
+      style={{ backgroundImage: 'url("/photos/HL.png")' }}
     >
       <h1 className="text-4xl font-bold">
         Where Good Food Meets Great Company - The Hungry Llama
