c409bdc3a4499c5bc6db1d6413e1633da155eb88
Ingi Haralds <ingiharaldss@gmail.com>
2024-10-08 22:58:48 +0000

    read me

--- shreknextcv/src/app/components/footer.tsx
+++ shreknextcv/src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-green-700 text-white p-4 mt-8">
-      <p>© {new Date().getFullYear()} Shreks CV. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Shreks CV. All Ogres reserved.</p>
     </footer>
   );
 };
