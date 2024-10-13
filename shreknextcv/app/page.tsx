a58c8d516fdf89bf80ade84981bdeb79aa56bb9f
Ingi Haralds <ingiharaldss@gmail.com>
2024-10-13 11:55:38 +0000

    break points á aðalsíðuna

--- shreknextcv/app/page.tsx
+++ shreknextcv/app/page.tsx
@@ -7,61 +7,59 @@ import ContactInformation from "../src/components/ContactInformation";
 import SkillsSection from "../src/components/SkillsSection";
 import WorkExperienceSection from "../src/components/WorkExperienceSection";
 import EducationSection from "../src/components/EducationSection";
-import PageHeader from "@/components/PageHeader";  // Make sure this path is correct
+import PageHeader from "@/components/PageHeader"; 
 
 const Home: React.FC = () => {
   return (
     <>
-      {/* Page Header*/}
+      {/* Page Header */}
       <PageHeader />
 
       <div className="min-h-screen flex flex-col">
         <main className="flex-grow p-8">
-         
-
           <PhotoUpload />
 
-          <br />
-          <br />
+          {/* Break points, svo þetta sjáist betur á síðunni */}
+          <div className="mt-8 sm:mt-10 md:mt-12 lg:mt-16 xl:mt-20"></div>
 
           {/* Contact Info */}
-          <div className="text-left bg-orange-500 border rounded ">
+          <div className="text-left bg-orange-500 border rounded p-4">
             <SectionHeader title="Contact Information" />
             <ContactInformation />
           </div>
-
-          <br />
-          <br />
+          
+          {/* Break points, svo þetta sjáist betur á síðunni */}
+          <div className="mt-8 sm:mt-10 md:mt-12 lg:mt-16 xl:mt-20"></div>
 
           {/* Skills */}
-          <div className="text-left bg-orange-500 border rounded ">
+          <div className="text-left bg-orange-500 border rounded p-4">
             <SectionHeader title="Skills" />
             <SkillsSection />
           </div>
 
-          <br />
-          <br />
+          {/* Break points, svo þetta sjáist betur á síðunni */}
+          <div className="mt-8 sm:mt-10 md:mt-12 lg:mt-16 xl:mt-20"></div>
 
           {/* Work Experience */}
-          <div className="bg-orange-500 border rounded ">
+          <div className="bg-orange-500 border rounded p-4">
             <SectionHeader title="Work Experience" />
             <WorkExperienceSection />
           </div>
 
-          <br />
-          <br />
+          {/* Break points, svo þetta sjáist betur á síðunni */}
+          <div className="mt-8 sm:mt-10 md:mt-12 lg:mt-16 xl:mt-20"></div>
 
           {/* Education */}
-          <div className="bg-orange-500 border rounded ">
+          <div className="bg-orange-500 border rounded p-4">
             <SectionHeader title="Education" />
             <EducationSection />
           </div>
 
-          <br />
-          <br />
+          {/* Break points, svo þetta sjáist betur á síðunni */}
+          <div className="mt-8 sm:mt-10 md:mt-12 lg:mt-16 xl:mt-20"></div>
 
           {/* About Me */}
-          <div className="bg-orange-500 border rounded ">
+          <div className="bg-orange-500 border rounded p-4">
             <SectionHeader title="About Me" />
             <AboutSection />
           </div>
