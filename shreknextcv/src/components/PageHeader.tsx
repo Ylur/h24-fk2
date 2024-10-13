a554daee9f2bef4d4fd84663f392cb99742f2dad
Ingi Haralds <ingiharaldss@gmail.com>
2024-10-13 11:25:23 +0000

    ReadMe og About

--- shreknextcv/src/components/PageHeader.tsx
+++ shreknextcv/src/components/PageHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageHeader: React.FC = () => {
   return (
-    <footer className="bg-green-600 text-white p-4 text-center mt-8">
+    <footer className="bg-green-600 text-white p-20 text-center mt-8">
       <p className="text-bold">Shreks CV.</p>
     </footer>
   );
