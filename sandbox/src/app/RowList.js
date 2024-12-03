8e8a4a8ce773b10ce442637d2b65ce2756053406
Ingi Haralds <ingiharaldss@gmail.com>
2024-12-03 19:02:57 +0000

    rowlist

---
+++ sandbox/src/app/RowList.js
@@ -0,0 +1,19 @@
+'use client'
+
+
+export function RowList({ children }) {
+    return (
+      <div className="RowList">
+        {children}
+      </div>
+    );
+  }
+  
+  export function Row({ children }) {
+    return (
+      <div className="Row">
+        {children}
+      </div>
+    );
+  }
+  
---
+++ sandbox/src/app/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { RowList, Row } from './RowList.js';
+
+export default function App() {
+  return (
+    <RowList>
+      <Row>
+        <p>This is the first item.</p>
+      </Row>
+      <Row>
+        <p>This is the second item.</p>
+      </Row>
+      <Row>
+        <p>This is the third item.</p>
+      </Row>
+    </RowList>
+  );
+}
